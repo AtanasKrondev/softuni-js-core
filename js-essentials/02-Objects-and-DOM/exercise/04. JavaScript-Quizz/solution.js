23993e331b3b96d936fba5ce37680507fcf4f660
Atanas Krondev <krondev@gmail.com>
2019-05-22 17:05:40 +0300

    01.	Cards

--- js-essentials/02-Objects-and-DOM/exercise/04. JavaScript-Quizz/solution.js
+++ js-essentials/02-Objects-and-DOM/exercise/04. JavaScript-Quizz/solution.js
@@ -1,24 +1,34 @@
 function solve() {
   let questionsSection = document.getElementsByTagName('section');
-  let wrongAnswers = document.getElementsByClassName('low-value');
-  let rightgAnswers = document.getElementsByClassName('high-value');
+  let wrongAnswers = Array.from(document.getElementsByClassName('low-value'));
+  let rightAnswers = Array.from(document.getElementsByClassName('high-value'));
   let countQuestions = 0;
   let countRight = 0;
-  let resultDisplay = document.querySelectorAll('#quizzie ul');
-  
-  
 
-
-
-  [...wrongAnswers].forEach(e => e.addEventListener('click', () => cicleQuestions()));
-  [...rightgAnswers].forEach(e => e.addEventListener('click', () => { cicleQuestions(); countRight++ }));
+  wrongAnswers.forEach(e => e.addEventListener('click', () => cicleQuestions()));
+  rightAnswers.forEach(e => e.addEventListener('click', () => { countRight++; cicleQuestions() }));
 
   function cicleQuestions() {
     questionsSection[countQuestions].className = 'hidden';
+    questionsSection[countQuestions].style.display = 'none';
+        
     countQuestions++;
     if (countQuestions < questionsSection.length) {
       questionsSection[countQuestions].className = '';
+      questionsSection[countQuestions].style.display = 'block';
+    } else {
+      let resultElement = document.getElementsByClassName('results-inner')[0].children[0];
+
+      if (countRight === 3) {
+        resultElement.textContent = 'You are recognized as top JavaScript fan!';
+      } else {
+        resultElement.textContent = `You have ${countRight} right answers`;
+      }
+
+      let resultDisplay = document.querySelectorAll('#quizzie ul')[3];
+      resultDisplay.style.display = 'block';
     }
+
   }
 
 }
