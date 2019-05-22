7cb0e3ef77498ea804c97e1af1db529b5c1d0df8
Atanas Krondev <krondev@gmail.com>
2019-05-22 12:57:05 +0300

    03. Number-Convertor

--- js-essentials/02-Objects-and-DOM/exercise/03. Number-Convertor/solution.js
+++ js-essentials/02-Objects-and-DOM/exercise/03. Number-Convertor/solution.js
@@ -1,4 +1,25 @@
 function solve() {
+    let toOptions = document.getElementById('selectMenuTo');
+    let convertBtn = document.getElementsByTagName('button')[0];
 
-    //TODO...
+    let option1 = document.createElement('option');
+    option1.value = 'binary';
+    option1.text = 'Binary';
+    toOptions.appendChild(option1);
+    let option2 = document.createElement('option');
+    option2.value = 'hexadecimal';
+    option2.text = 'Hexadecimal';
+    toOptions.appendChild(option2);
+
+    convertBtn.addEventListener('click', () => {
+        let inputNumber = Number(document.getElementsByTagName('input')[0].value);
+        let convertingTo = toOptions.options[toOptions.selectedIndex].value;
+
+        if (convertingTo === 'binary') {
+            let result = document.getElementsByTagName('input')[1].value = inputNumber.toString(2);
+        } else if (convertingTo === 'hexadecimal') {
+            let result = document.getElementsByTagName('input')[1].value = inputNumber.toString(16).toUpperCase();
+        }
+    })
+
 }
--- js-essentials/02-Objects-and-DOM/exercise/04. JavaScript-Quizz/solution.js
+++ js-essentials/02-Objects-and-DOM/exercise/04. JavaScript-Quizz/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let questionsSection = document.getElementsByTagName('section');
+  let wrongAnswers = document.getElementsByClassName('low-value');
+  let rightgAnswers = document.getElementsByClassName('high-value');
+  let countQuestions = 0;
+  let countRight = 0;
+  let resultDisplay = document.querySelectorAll('#quizzie ul');
+  
+  
+
+
+
+  [...wrongAnswers].forEach(e => e.addEventListener('click', () => cicleQuestions()));
+  [...rightgAnswers].forEach(e => e.addEventListener('click', () => { cicleQuestions(); countRight++ }));
+
+  function cicleQuestions() {
+    questionsSection[countQuestions].className = 'hidden';
+    countQuestions++;
+    if (countQuestions < questionsSection.length) {
+      questionsSection[countQuestions].className = '';
+    }
+  }
+
 }
