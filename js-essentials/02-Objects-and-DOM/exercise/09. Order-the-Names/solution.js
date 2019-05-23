87f232d0050501cb7290c750c6a9700850a29cbc
Atanas Krondev <krondev@gmail.com>
2019-05-23 15:17:54 +0300

    Update solution.js

--- js-essentials/02-Objects-and-DOM/exercise/09. Order-the-Names/solution.js
+++ js-essentials/02-Objects-and-DOM/exercise/09. Order-the-Names/solution.js
@@ -1,4 +1,21 @@
 function solve() {
 
-    //TODO...
+    let button = document.getElementsByTagName('button')[0];
+    let input = document.getElementsByTagName('input')[0];
+    let li = Array.from(document.getElementsByTagName('li'));
+
+    button.addEventListener('click', function () {
+        let name = input.value;
+        pushToDatabase(name);
+    })
+
+    function pushToDatabase(str) {
+        str = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+        let i = str.charCodeAt(0) - 65;
+        let arr = li[i].textContent.split(', ').filter(n => n);
+
+        arr.push(str);
+        li[i].textContent = arr.join(', ');
+    }
 }
