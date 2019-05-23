a1e8e8116233001eef32cf842c642d5ff2840171
Atanas Krondev <krondev@gmail.com>
2019-05-23 10:59:35 +0300

    05.	Table – Search Engine

--- js-essentials/02-Objects-and-DOM/exercise/05. Table-Search-Engine/solution.js
+++ js-essentials/02-Objects-and-DOM/exercise/05. Table-Search-Engine/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-   //TODO...
+   let tableContent = Array.from(document.getElementsByTagName('td'));
+   let button = document.getElementById('searchBtn');
+   let searchField = document.getElementById('searchField');
+
+   button.addEventListener('click', search);
+
+   function search() {
+      let searchStr = searchField.value;
+
+      if (searchStr !== '') {
+         for (let cell of tableContent) {
+            if (cell.textContent.includes(searchStr)) {
+               cell.parentNode.className = 'select';
+            }
+         }
+      }
+
+      searchField.value = '';
+   }
 }
