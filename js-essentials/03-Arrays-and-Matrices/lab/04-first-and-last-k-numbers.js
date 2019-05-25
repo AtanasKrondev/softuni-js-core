c727634a4a18c2fb3df582d15b7c5bb4195b2913
Atanas Krondev <krondev@gmail.com>
2019-05-26 01:00:22 +0300

    Update 04-first-and-last-k-numbers.js

--- js-essentials/03-Arrays-and-Matrices/lab/04-first-and-last-k-numbers.js
+++ js-essentials/03-Arrays-and-Matrices/lab/04-first-and-last-k-numbers.js
@@ -1,5 +1,15 @@
 function solve(arr) {
-    
+    let k = arr.shift();
+    let first = [];
+    let last = [];
+
+    for (let i = 0; i < k; i++) {
+        first.push(arr[i]);
+    }
+
+    for (let i = arr.length - k; i < arr.length; i++) {
+        last.push
+    }
 }
 
-solve()
+solve([2, 7, 8, 9]);
