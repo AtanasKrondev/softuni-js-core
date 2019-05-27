1a99b8e45159b13fdd0d99306203c50ba5f67903
Atanas Krondev <krondev@gmail.com>
2019-05-27 11:27:50 +0300

    Create 06-sort-array.js

---
+++ js-essentials/03-Arrays-and-Matrices/exercise/06-sort-array.js
@@ -0,0 +1,8 @@
+function sortByTwoCriterias(arr) {
+    arr.sort((a, b) => a.length - b.length || a.toLowerCase().localeCompare(b.toLowerCase()))
+        .forEach(e => console.log(e))
+}
+
+sortByTwoCriterias(['alpha',
+    'beta',
+    'gamma'])
