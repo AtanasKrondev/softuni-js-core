f8aff96b1422101d996dfca82a3b4ddcb990a464
Atanas Krondev <krondev@gmail.com>
2019-05-27 11:28:35 +0300

    Update 06-sort-array.js

--- js-essentials/03-Arrays-and-Matrices/exercise/06-sort-array.js
+++ js-essentials/03-Arrays-and-Matrices/exercise/06-sort-array.js
@@ -1,5 +1,7 @@
 function sortByTwoCriterias(arr) {
-    arr.sort((a, b) => a.length - b.length || a.toLowerCase().localeCompare(b.toLowerCase()))
+    arr.sort((a, b) =>
+        a.length - b.length ||
+        a.toLowerCase().localeCompare(b.toLowerCase()))
         .forEach(e => console.log(e))
 }
 
