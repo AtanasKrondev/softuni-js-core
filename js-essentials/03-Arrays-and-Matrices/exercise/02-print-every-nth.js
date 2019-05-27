7511b669d596f400fe0b0c176facf69c6f7bbfd0
Atanas Krondev <krondev@gmail.com>
2019-05-27 11:04:05 +0300

    Create 02-print-every-nth.js

---
+++ js-essentials/03-Arrays-and-Matrices/exercise/02-print-every-nth.js
@@ -0,0 +1,14 @@
+function everyN(arr) {
+    let step = +arr.pop();
+
+    for (let i = 0; i < arr.length; i += step) {
+        console.log(arr[i]);
+    }
+}
+
+everyN(['5',
+    '20',
+    '31',
+    '4',
+    '20',
+    '2']);
