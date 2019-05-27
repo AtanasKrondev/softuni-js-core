d1b16b1c92c6fc517f87c499b8e993108df69a89
Atanas Krondev <krondev@gmail.com>
2019-05-27 10:16:24 +0300

    Create 10-equal-neighbours.js

---
+++ js-essentials/03-Arrays-and-Matrices/10-equal-neighbours.js
@@ -0,0 +1,25 @@
+function solve(matrix) {
+    let result = 0;
+
+    for (let row = 1; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length - 1; col++) {
+            if (matrix[row][col] === matrix[row - 1][col]) {
+                result++;
+            }
+        }
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 1; col < matrix[row].length; col++) {
+            if (matrix[row][col] === matrix[row][col - 1]) {
+                result++;
+            }
+        }
+    }
+
+    console.log(result);
+}
+
+solve([['test', 'yes', 'yo', 'ho'],
+['well', 'done', 'yo', '6'],
+['not', 'done', 'yet', '5']]);
