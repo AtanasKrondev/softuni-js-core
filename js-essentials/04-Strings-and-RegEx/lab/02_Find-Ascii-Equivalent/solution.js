834abb0faaaef828f9339f0e4fc510de55bbe13b
Atanas Krondev <krondev@gmail.com>
2019-05-28 12:29:25 +0300

    Update solution.js

--- js-essentials/04-Strings-and-RegEx/lab/02_Find-Ascii-Equivalent/solution.js
+++ js-essentials/04-Strings-and-RegEx/lab/02_Find-Ascii-Equivalent/solution.js
@@ -1,4 +1,30 @@
 function solve() {
+  let input = document.getElementById('text').value;
+  let button = document.querySelector('input[type="button"]');
 
-  //TODO...
+  button.addEventListener('click', convertToAscii(input));
+
+  function convertToAscii(input) {
+    let split = input.split(' ').filter(e => e !== '');
+    let word = '';
+    let result = document.getElementById('result');
+    for (let element of split) {
+      if (Number(element)) {
+        word += String.fromCharCode(element);
+      } else {
+        let wordsToAscii = [];
+
+        for (let letter of element) {
+          wordsToAscii.push(letter.charCodeAt(0));
+        }
+        let p = document.createElement('p');
+        p.textContent = wordsToAscii.join(' ');
+        result.appendChild(p);
+      }
+    }
+
+    let p = document.createElement('p');
+    p.textContent = word;
+    result.appendChild(p);
+  }
 }
