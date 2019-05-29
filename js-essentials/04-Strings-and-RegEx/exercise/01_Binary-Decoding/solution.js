4e26486238ec65efd1850351b22776260154ce6e
Atanas Krondev <krondev@gmail.com>
2019-05-29 17:11:24 +0300

    Update solution.js

--- js-essentials/04-Strings-and-RegEx/exercise/01_Binary-Decoding/solution.js
+++ js-essentials/04-Strings-and-RegEx/exercise/01_Binary-Decoding/solution.js
@@ -4,30 +4,31 @@ function solve() {
     let result = document.getElementById('resultOutput');
     let resultBox = document.getElementsByClassName('boxes')[0];
 
-    button.addEventListener('click', decode(input));
+    let decoded = '';
+    let weightNum = input.split('').reduce((a, b) => +a + +b);
+    let weightStr = weightNum.toString();
 
-    function decode(input) {
-        let decoded = '';
-        let weightNum = input.split('').reduce((a, b) => +a + +b);
-        let weightStr = weightNum.toString();
-
-        while (weightStr.length > 1) {
-            weightNum = 0;
-            for (let digit of weightStr) {
-                weightNum += +digit;
-            }
-            weightStr = weightNum.toString();
+    while (weightStr.length > 1) {
+        weightNum = 0;
+        for (let digit of weightStr) {
+            weightNum += +digit;
         }
-
-        input = input.slice(weightNum);
-        input = input.substring(0, input.length - weightNum);
-
-        let arr = input.match(/.{1,8}/g);
-        arr.forEach(e => {
-            decoded += String.fromCharCode(parseInt(e, 2))
-        });
-
-        resultBox.style.display = 'flex';
-        result.textContent = decoded;
+        weightStr = weightNum.toString();
     }
+
+    input = input.slice(weightNum);
+    input = input.substring(0, input.length - weightNum);
+
+    let arr = input.match(/.{1,8}/g);
+    arr.forEach(e => {
+        let c = String.fromCharCode(parseInt(e, 2))
+        if (/[A-Za-z ]+/g.test(c)) {
+            decoded += c;
+        }
+    });
+
+    let validChars = decoded.split('').filter(c => /[A-Za-z ]+/g.test(c))
+
+    resultBox.style.display = 'flex';
+    result.textContent = decoded;
 }
