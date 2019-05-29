fd937d6ded2e6fe0da2608a3ea838bb9dc824462
Atanas Krondev <krondev@gmail.com>
2019-05-29 11:22:57 +0300

    Update solution.js

--- js-essentials/04-Strings-and-RegEx/lab/04_Replace-a-Certain-Word/solution.js
+++ js-essentials/04-Strings-and-RegEx/lab/04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,19 @@
 function solve() {
+    let word = document.getElementById('word').value;
+    let text = document.getElementById('text').value;
+    let button = document.querySelector('input[type="button"]');
+    let result = document.getElementById('result');
 
-    //TODO...
+    button.addEventListener('click', replace(word, text));
+
+    function replace(word, text) {
+        let arr = JSON.parse(text);
+        let toReplace = new RegExp(arr[0].split(' ')[2].toLowerCase(), 'i');
+
+        arr.forEach(e => {
+            let p = document.createElement('p');
+            p.textContent = e.replace(toReplace, word);
+            result.appendChild(p);
+        });
+    }
 }
