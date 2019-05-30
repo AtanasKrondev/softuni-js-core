bf3c4021730051fb580afcba674ac4240d496055
Atanas Krondev <krondev@gmail.com>
2019-05-30 15:49:42 +0300

    Update solution.js

--- js-essentials/04-Strings-and-RegEx/exercise/03_James-Bond/solution.js
+++ js-essentials/04-Strings-and-RegEx/exercise/03_James-Bond/solution.js
@@ -1,8 +1,33 @@
 function solve() {
-    let [key, text] = JSON.parse(document.getElementById('array').value);
+    let [key, ...textInput] = JSON.parse(document.getElementById('array').value);
     let result = document.getElementById('result');
+    let splitByKey = new RegExp(`(?<= |^)${key}`, 'i');
+    let encodingPattern = /^ +[A-Z!%$#]{8,}(?= |\.|\,)/;
 
-    
+    for (let text of textInput) {
+        let textArr = text.split(splitByKey);
+        let matchesArr = [];
+
+        for (let i = 1; i < textArr.length; i++) {
+            if (textArr[i].match(encodingPattern) !== null) {
+                matchesArr.push(textArr[i].match(encodingPattern)[0]);
+            }
+        }
+
+        let resultText = text;
+
+        matchesArr.forEach(e => {
+            let substitute = e
+                .toLowerCase()
+                .replace(/!/g, '1')
+                .replace(/%/g, '2')
+                .replace(/\#/g, '3')
+                .replace(/\$/g, '4')
+            resultText = resultText.replace(e, substitute);
+        })
+
+        appendP(resultText)
+    }
 
     function appendP(str) {
         let p = document.createElement('p');
