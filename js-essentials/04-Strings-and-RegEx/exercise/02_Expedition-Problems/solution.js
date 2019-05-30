c5949197d9705eef4d9da76db96c285edc021436
Atanas Krondev <krondev@gmail.com>
2019-05-30 11:47:37 +0300

    Update solution.js

--- js-essentials/04-Strings-and-RegEx/exercise/02_Expedition-Problems/solution.js
+++ js-essentials/04-Strings-and-RegEx/exercise/02_Expedition-Problems/solution.js
@@ -1,4 +1,35 @@
 function solve() {
+    let string = document.getElementById('string').value;
+    let text = document.getElementById('text').value;
+    let result = document.getElementById('result');
+    let regex = /(east|north).*?([0-9]{2})[^,]*?,[^,]*?([0-9]{6})/;
+    let coordinates = []
 
-    //TODO...
+    let matches = text.match(new RegExp(regex, 'gi')).forEach(e => {
+        let match = new RegExp(regex, 'i').exec(e);
+        coordinates.push(`${match[2]}.${match[3]} ${match[1][0].toUpperCase()}`);
+    });
+
+    let north = '';
+    let east = '';
+
+    for (let el of coordinates) {
+        if (el.includes('N')) {
+            north = el;
+        } else if (el.includes('E')) {
+            east = el;
+        }
+    }
+
+    let message = text.split(string).splice(1, 1);
+
+    appendP(north);
+    appendP(east);
+    appendP(`Message: ${message}`);
+
+    function appendP(str) {
+        let p = document.createElement('p');
+        p.textContent = str;
+        result.appendChild(p);
+    }
 }
