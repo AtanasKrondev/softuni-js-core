714517ccc9002aa70161b756463f0d714acab8ff
Atanas Krondev <krondev@gmail.com>
2019-05-31 11:16:19 +0300

    Update solution.js

--- js-essentials/04-Strings-and-RegEx/exercise/03_James-Bond/solution.js
+++ js-essentials/04-Strings-and-RegEx/exercise/03_James-Bond/solution.js
@@ -1,37 +1,34 @@
 function solve() {
     let [key, ...textInput] = JSON.parse(document.getElementById('array').value);
     let result = document.getElementById('result');
-    let splitByKey = new RegExp(`(?<= |^)${key}`, 'i');
-    let encodingPattern = /^ +[A-Z!%$#]{8,}(?= |\.|\,)/;
+
+    let pattern = new RegExp(`(\\s|^)(${key}\\s+)([A-Z!#$%]{8,})(\\.|,|\\s|$)`, 'gi');
 
     for (let text of textInput) {
-        let textArr = text.split(splitByKey);
-        let matchesArr = [];
+        let match = pattern.exec(text);
 
-        for (let i = 1; i < textArr.length; i++) {
-            if (textArr[i].match(encodingPattern) !== null) {
-                matchesArr.push(textArr[i].match(encodingPattern)[0]);
+        while (match) {
+            if (match[3] === match[3].toUpperCase()) {
+                text = text.replace(match[0], match[1] + match[2] + decodeMessage(match[3]) + match[4])
             }
+
+            match = pattern.exec(text);
         }
 
-        let resultText = text;
-
-        matchesArr.forEach(e => {
-            let substitute = e
-                .toLowerCase()
-                .replace(/!/g, '1')
-                .replace(/%/g, '2')
-                .replace(/\#/g, '3')
-                .replace(/\$/g, '4')
-            resultText = resultText.replace(e, substitute);
-        })
-
-        appendP(resultText)
+        appendP(text);
     }
 
     function appendP(str) {
         let p = document.createElement('p');
         p.textContent = str;
         result.appendChild(p);
+    }
+
+    function decodeMessage(str) {
+        return str.toLowerCase()
+            .replace(/!/g, '1')
+            .replace(/%/g, '2')
+            .replace(/\#/g, '3')
+            .replace(/\$/g, '4')
     }
 }
