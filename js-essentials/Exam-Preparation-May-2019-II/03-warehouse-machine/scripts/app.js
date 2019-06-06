d23e61e71430309689c7d7927250225f0b46380a
Atanas Krondev <krondev@gmail.com>
2019-06-06 12:33:21 +0300

    Update app.js

--- js-essentials/Exam-Preparation-May-2019-II/03-warehouse-machine/scripts/app.js
+++ js-essentials/Exam-Preparation-May-2019-II/03-warehouse-machine/scripts/app.js
@@ -5,11 +5,37 @@ function coffeeStorage() {
 
     for (const command of inputElement) {
         if (command === 'REPORT') {
-            // reportElement.innerHTML = storage.join('<br>');   
-            console.log(storage);
-                     
+            let reportArr = [];
+
+            let arrayStorage = Object.entries(storage);
+            arrayStorage.forEach(([brand, coffee]) => {
+                let reportCoffeeArr = [];
+                let arrayBrand = Object.entries(coffee);
+                arrayBrand.forEach(([coffee, info]) => {
+                    reportCoffeeArr.push(`${coffee} - ${info.expDate} - ${info.qty}.`)
+                })
+
+                let reportStr = `${brand}: ${reportCoffeeArr.join(' ')}`;
+                reportArr.push(reportStr);
+            })
+
+            reportElement.innerHTML = reportArr.join('<br>');
         } else if (command === 'INSPECTION') {
-            inspectElement.textContent = 'makari';
+            let reportArr = [];
+
+            let arrayStorage = Object.entries(storage).sort((a, b) => a[0].localeCompare(b[0]));
+            arrayStorage.forEach(([brand, coffee]) => {
+                let reportCoffeeArr = [];
+                let arrayBrand = Object.entries(coffee).sort((a, b) => b[1].qty - a[1].qty);
+                arrayBrand.forEach(([coffee, info]) => {
+                    reportCoffeeArr.push(`${coffee} - ${info.expDate} - ${info.qty}.`)
+                })
+
+                let reportStr = `${brand}: ${reportCoffeeArr.join(' ')}`;
+                reportArr.push(reportStr);
+            })
+
+            inspectElement.innerHTML = reportArr.join('<br>');
         } else {
             let [action, brand, coffee, expDate, qty] = command.split(', ');
             if (action === 'IN') {
@@ -18,10 +44,22 @@ function coffeeStorage() {
                 }
 
                 if (!storage[brand].hasOwnProperty(coffee)) {
-                    storage[brand][coffee] = { expDate, qty };
+                    storage[brand][coffee] = { expDate, qty: +qty };
+                } else {
+                    if (storage[brand][coffee].expDate < expDate) {
+                        storage[brand][coffee].expDate = expDate;
+                        storage[brand][coffee].qty = +qty;
+                    } else if (storage[brand][coffee].expDate === expDate) {
+                        storage[brand][coffee].qty += +qty;
+                    }
                 }
             } else if (action === 'OUT') {
-
+                if (storage.hasOwnProperty(brand)
+                    && storage[brand].hasOwnProperty(coffee)
+                    && storage[brand][coffee].expDate > expDate
+                    && storage[brand][coffee].qty >= +qty) {
+                    storage[brand][coffee].qty -= +qty;
+                }
             }
         }
     }
