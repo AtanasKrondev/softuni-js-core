f45046c7e930f0c5e80b3712adf0b67eeae854dd
Atanas Krondev <krondev@gmail.com>
2019-06-11 15:00:14 +0300

    Animated Dot

--- js-essentials/Workshop-SoftUni-Invaders/scripts/game.js
+++ js-essentials/Workshop-SoftUni-Invaders/scripts/game.js
@@ -1,26 +1,46 @@
 (function () {
-    const initRenderer = function (ctx) {
+    const initRenderer = function (ctx, bounds) {
         this.ctx = ctx;
+        this.bounds = bounds;
         return this;
     };
 
-    const rendererDot = function (ctx, left, top) {
+    const clear = function () {
+        const { ctx } = this;
+        const { width, height } = this.bounds;
+        ctx.clearRect(0, 0, width, height);
+    };
+
+    const rendererDot = function (left, top) {
+        const { ctx } = this;
         ctx.fillRect(left, top, 15, 15);
     };
 
     const renderer = {
+        init: initRenderer,
         rendererDot,
+        clear,
+        //renderPlayer
+        //renderBullets
+        //renderEnemies
     };
 
 
-    const init = (selector, width, height) => {
+    const init = function (selector, width, height) {
         const gameContainer = document.querySelector(selector);
         const canvas = document.createElement('canvas');
         canvas.width = width;
         canvas.height = height;
+
         gameContainer.appendChild(canvas);
+
         const ctx = canvas.getContext('2d');
-        renderer.rendererDot(ctx, 0, 100);
+        this.bounds = {
+            width,
+            height,
+        };
+
+        this.renderer = renderer.init(ctx, this.bounds);
         return this;
     };
 
@@ -29,10 +49,17 @@
         top: 100
     }
 
+    let iteration = 0;
     const gameLoop = function () {
+        this.renderer.clear();
         const { left, top } = dot;
         this.renderer.rendererDot(left, top);
-    }
+        const alpha = Math.cos(iteration);
+        iteration++;
+        dot.left += 1;
+        dot.top += alpha * 5;
+        window.requestAnimationFrame(() => { this.gameLoop() });
+    };
 
     const start = function () {
         this.gameLoop();
