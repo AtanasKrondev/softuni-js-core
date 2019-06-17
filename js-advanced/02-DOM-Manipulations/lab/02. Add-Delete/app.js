296d9ef2acdca8f6b6516dd14e51ca01c92bc2b6
Atanas Krondev <krondev@gmail.com>
2019-06-17 15:43:10 +0300

    Update app.js

--- js-advanced/02-DOM-Manipulations/lab/02. Add-Delete/app.js
+++ js-advanced/02-DOM-Manipulations/lab/02. Add-Delete/app.js
@@ -1,3 +1,29 @@
 function addItem() {
-    //TODO...
+    const text = document.getElementById('newText').value;
+    const li = buildElement('li', `${text} `)
+    const listItem = document.getElementById('items');
+    listItem.appendChild(li);
+    const eListener = {
+        type: 'click',
+        func: deleteElement,
+    }
+    const deleteBtn = buildElement('a', '[Delete]', { name: 'href', value: '#' }, eListener)
+    li.appendChild(deleteBtn)
+    function deleteElement() {
+        const element = this.parentNode;
+        element.remove();
+    }
+
+
+    function buildElement(tag, text, attr, eListener) {
+        const elementTag = document.createElement(tag)
+        elementTag.textContent = text;
+        if (attr) {
+            elementTag.setAttribute(attr.name, attr.value);
+        }
+        if (eListener) {
+            elementTag.addEventListener(eListener.type, eListener.func)
+        }
+        return elementTag
+    }
 }
