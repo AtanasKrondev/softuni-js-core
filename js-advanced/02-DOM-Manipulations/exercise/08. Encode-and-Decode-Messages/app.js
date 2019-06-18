cad1ba9d9ee4260b04b816ee5b25ba114f112875
Atanas Krondev <krondev@gmail.com>
2019-06-18 15:57:48 +0300

    Update app.js

--- js-advanced/02-DOM-Manipulations/exercise/08. Encode-and-Decode-Messages/app.js
+++ js-advanced/02-DOM-Manipulations/exercise/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,29 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [sendBtn, decodeBtn] = [...document.getElementsByTagName('button')];
+    const [sendArea, decodeArea] = [...document.getElementsByTagName('textarea')];
+
+    sendBtn.addEventListener('click', function () {
+        let input = sendArea.value;
+        let encodeMessage = '';
+
+        for (const i of input) {
+            encodeMessage += String.fromCharCode(i.charCodeAt(0) + 1);
+        }
+
+        decodeArea.value = encodeMessage;
+        sendArea.value = '';
+
+    })
+
+    decodeBtn.addEventListener('click', function () {
+        let input = decodeArea.value;
+        let decodeMessage = '';
+
+        for (const i of input) {
+            decodeMessage += String.fromCharCode(i.charCodeAt(0) - 1);
+        }
+
+        decodeArea.value = decodeMessage;
+    })
+
 }
