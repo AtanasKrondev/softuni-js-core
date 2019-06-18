08b56e0f8d1f2c6f61bbbd79dc8dcd4e774a4b4e
Atanas Krondev <krondev@gmail.com>
2019-06-18 15:30:32 +0300

    Update script.js

--- js-advanced/02-DOM-Manipulations/exercise/04. Sections/script.js
+++ js-advanced/02-DOM-Manipulations/exercise/04. Sections/script.js
@@ -1,3 +1,16 @@
 function create(words) {
-   console.log('TODO:...');
+   const container = document.getElementById('content');
+   for (const word of words) {
+      const divEl = document.createElement('div');
+      const pEl = document.createElement('p');
+
+      pEl.textContent = word;
+      pEl.style.display = 'none';
+
+      divEl.appendChild(pEl);
+      divEl.addEventListener('click', () => {
+         pEl.style.display = 'inline-block';
+      });
+      container.appendChild(divEl);
+   }
 }
