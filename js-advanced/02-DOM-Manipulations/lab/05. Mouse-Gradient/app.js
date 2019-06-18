8f3aca5fcad05838c0136f262a94d50d76d79dd7
Atanas Krondev <krondev@gmail.com>
2019-06-18 12:41:07 +0300

    Mouse Gradient

--- js-advanced/02-DOM-Manipulations/lab/05. Mouse-Gradient/app.js
+++ js-advanced/02-DOM-Manipulations/lab/05. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const result = document.getElementById('result');
+    gradient.addEventListener('mousemove', trackMouse);
+
+    function trackMouse(event) {
+        console.log(typeof event.target.clientWidth);
+
+        const targetWidth = +event.target.clientWidth;
+        const positionX = +event.offsetX;
+        const asPercent = Math.floor((positionX / targetWidth) * 100);
+        result.textContent = `${asPercent}%`;
+    }
 }
--- js-advanced/02-DOM-Manipulations/lab/07. Dynamic-Validation/app.js
+++ js-advanced/02-DOM-Manipulations/lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.getElementById('email');
+
+    input.addEventListener('change', dynamicValidation);
+
+    function dynamicValidation(event) {
+        const emailStr = event.target.value;
+        const regEx = /^[a-z]+\@[a-z]+\.[a-z]+$/;
+        if (!regEx.test(emailStr)) {
+            input.setAttribute('class', 'error');
+        }
+        if (regEx.test(emailStr)) {
+            input.removeAttribute('class');
+        }
+    }
 }
