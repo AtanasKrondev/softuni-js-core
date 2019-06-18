ec8c7b55f33a07d3abb2d0091e96f786c67c1946
Atanas Krondev <krondev@gmail.com>
2019-06-18 10:01:19 +0300

    Stopwatch

--- js-advanced/02-DOM-Manipulations/lab/03. Delete-From-Table/app.js
+++ js-advanced/02-DOM-Manipulations/lab/03. Delete-From-Table/app.js
@@ -1,3 +1,28 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const SELECTORS = {
+        SEARCHROW: 'Email',
+        INPUT: 'input[name="email"]',
+        RESULT: 'result',
+        ERROR: 'Not found.',
+        SUCCESS: 'Deleted.',
+
+    }
+    const input = document.querySelector(SELECTORS.INPUT).value;
+    const result = document.getElementById(SELECTORS.RESULT);
+    const tableHeads = document.getElementsByTagName('th');
+    const searchColumn = [...tableHeads].map(th => th.textContent).indexOf(SELECTORS.SEARCHROW) + 1;
+    SELECTORS.ROWSELECTOR = `#customers tr td:nth-child(${searchColumn})`;
+    let resultFound = false;
+    [...document.querySelectorAll(SELECTORS.ROWSELECTOR)]
+        .forEach(row => {
+            if (row.textContent === input) {
+                row.parentNode.remove();
+                resultFound = true;
+            }
+        });
+    if (resultFound) {
+        result.textContent = SELECTORS.SUCCESS;
+    } else {
+        result.textContent = SELECTORS.ERROR;
+    }
 }
--- js-advanced/02-DOM-Manipulations/lab/04. Stopwatch/stopwatch.js
+++ js-advanced/02-DOM-Manipulations/lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,48 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let seconds = 0;
+    let minutes = 0;
+    let time;
+    let displayTime = document.getElementById('time');
+    const start = document.getElementById('startBtn');
+    const stop = document.getElementById('stopBtn');
+
+    // start.onclick = starTime;
+    // stop.onclick = stopTime;
+    start.addEventListener('click', starTime);
+    stop.addEventListener('click', stopTime);
+
+    function starTime() {
+        start.disabled = true;
+        stop.disabled = false;
+        ticToc();
+    }
+
+    function stopTime() {
+        stop.disabled = true;
+        start.disabled = false;
+        clearTimeout(time);
+        seconds = 0;
+        minutes = 0;
+        // displayTime.textContent = displayingTime(minutes, seconds);
+    }
+
+
+    function ticToc() {
+        displayTime.textContent = displayingTime(minutes, seconds);
+        ++seconds;
+
+        if (seconds === 60) {
+            ++minutes;
+            seconds = 0;
+        }
+        timer();
+    }
+
+    function timer() {
+        time = setTimeout(ticToc, 1000)
+    }
+
+    function displayingTime(minutes, seconds) {
+        return `${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}`;
+    }
 }
