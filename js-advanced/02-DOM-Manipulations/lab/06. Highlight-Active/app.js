8d3682bd4c0d689474abe925221ef18a4a4125df
Atanas Krondev <krondev@gmail.com>
2019-06-18 11:51:20 +0300

    Update app.js

--- js-advanced/02-DOM-Manipulations/lab/06. Highlight-Active/app.js
+++ js-advanced/02-DOM-Manipulations/lab/06. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focus() {
-    console.log('TODO:...');
+    let inputs = [...document.getElementsByTagName('input')];
+    inputs.forEach(input => input.addEventListener('focus', onFocus));
+    inputs.forEach(input => input.addEventListener('blur', onBlur));
+
+    function onFocus(event) {
+        let input = event.target;
+        input.parentNode.setAttribute('class', 'focused');
+    }
+
+    function onBlur(event) {
+        let input = event.target;
+        input.parentNode.removeAttribute('class');
+    }
 }
