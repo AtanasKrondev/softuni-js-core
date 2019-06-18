39709a90049e6faaa3c0d43e9459891116818ee3
Atanas Krondev <krondev@gmail.com>
2019-06-18 15:20:44 +0300

    Update accordion.js

--- js-advanced/02-DOM-Manipulations/exercise/03. Accordion/accordion.js
+++ js-advanced/02-DOM-Manipulations/exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.querySelector('.button');
+    let article = document.getElementById('extra');
+
+    if (article.style.display === 'none') {
+        article.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        article.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
