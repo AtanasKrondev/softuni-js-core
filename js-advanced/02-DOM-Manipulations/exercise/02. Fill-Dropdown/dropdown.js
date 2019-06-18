7a2b87030276b872efddcc621891e525b2f81be9
Atanas Krondev <krondev@gmail.com>
2019-06-18 15:13:37 +0300

    Update dropdown.js

--- js-advanced/02-DOM-Manipulations/exercise/02. Fill-Dropdown/dropdown.js
+++ js-advanced/02-DOM-Manipulations/exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const menuList = document.getElementById('menu');
+    const newText = document.getElementById('newItemText').value;
+    const newValue = document.getElementById('newItemValue').value;
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = newText;
+    optionElement.value = newValue;
+
+    menuList.appendChild(optionElement);
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
