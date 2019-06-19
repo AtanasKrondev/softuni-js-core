74594b7a95cf63359abc090c0e17e282c604898a
Atanas Krondev <krondev@gmail.com>
2019-06-19 10:28:32 +0300

    Time Converter

--- js-advanced/02-DOM-Manipulations/exercise/05. Notification/scripts.js
+++ js-advanced/02-DOM-Manipulations/exercise/05. Notification/scripts.js
@@ -1,3 +1,13 @@
 function notify(message) {
-    console.log('TODO:...');
+    const btn = document.querySelector('button');
+    const notification = document.getElementById('notification');
+    notification.textContent = message;
+    let t;
+    btn.addEventListener('click', notifyMe);
+
+    function notifyMe() {
+        notification.style.display = 'block';
+
+        t = setTimeout(function () { notification.style.display = 'none' }, 2000);
+    }
 }
--- js-advanced/02-DOM-Manipulations/exercise/06. Time-Converter/solution.js
+++ js-advanced/02-DOM-Manipulations/exercise/06. Time-Converter/solution.js
@@ -1,4 +1,36 @@
 function attachEventsListeners() {
+    const days = document.getElementById('days');
+    const hours = document.getElementById('hours');
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
 
-    console.log('TODO:...');
+    [...document.querySelectorAll('input[type="button"]')]
+        .forEach(btn => btn.addEventListener('click', convert));
+
+    function convert(event) {
+        const btnId = event.target.getAttribute('id');
+        switch (btnId) {
+            case 'daysBtn':
+                hours.value = days.value * 24;
+                minutes.value = hours.value * 60;
+                seconds.value = minutes.value * 60;
+                break;
+            case 'hoursBtn':
+                days.value = hours.value / 24;
+                minutes.value = hours.value * 60;
+                seconds.value = minutes.value * 60;
+                break;
+            case 'minutesBtn':
+                hours.value = minutes.value / 60;
+                days.value = hours.value / 24;
+                seconds.value = minutes.value * 60;
+                break;
+            case 'secondsBtn':
+                minutes.value = seconds.value / 60;
+                hours.value = minutes.value / 60;
+                days.value = hours.value / 24;
+        }
+
+    }
+
 }
