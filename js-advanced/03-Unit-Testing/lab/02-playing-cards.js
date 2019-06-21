5f1792f71fd9cf9062aa3b4febc90e355b24074f
Atanas Krondev <krondev@gmail.com>
2019-06-21 14:42:50 +0300

    Update 02-playing-cards.js

--- js-advanced/03-Unit-Testing/lab/02-playing-cards.js
+++ js-advanced/03-Unit-Testing/lab/02-playing-cards.js
@@ -2,9 +2,23 @@ function playingCards(face, suit) {
     const validFace = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     const validSuit = ['S', 'H', 'D', 'C'];
     if (validFace.includes(face) && validSuit.includes(suit)) {
-        return { face, suit }.toString()
-    }
+        let suitSymbol = () => {
+            switch (suit) {
+                case 'S': return `\u2660`;
+                case 'H': return `\u2665`;
+                case 'D': return `\u2666`;
+                case 'C': return `\u2663`;
+            }
+        }
+        return {
+            face,
+            suit: suitSymbol(),
+            toString: function () {
+                return `${this.face}${this.suit}`;
+            }
+        }
+    } else throw Error('Error');
 }
 
-console.log(playingCards('2', 'S'));
+console.log(playingCards('2', 'F').toString());
 
