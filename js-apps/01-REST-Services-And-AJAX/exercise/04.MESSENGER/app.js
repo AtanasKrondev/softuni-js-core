7ad12640dea7b1afeb5154bd7e916636286699a6
Atanas Krondev <krondev@gmail.com>
2019-07-14 13:47:43 +0300

    Update app.js

--- js-apps/01-REST-Services-And-AJAX/exercise/04.MESSENGER/app.js
+++ js-apps/01-REST-Services-And-AJAX/exercise/04.MESSENGER/app.js
@@ -1,5 +1,44 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'https://rest-messanger.firebaseio.com/messanger.json';
+    const messagesArea = document.getElementById('messages');
+
+    document.getElementById('submit').addEventListener('click', function () {
+        const author = document.getElementById('author').value;
+        const content = document.getElementById('content').value;
+
+        if (author && content) {
+            const data = { author, content };
+            fetch(url, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            }).then(() => refresh())
+                .then(() => {
+                    document.getElementById('author').value = '';
+                    document.getElementById('content').value = '';
+                })
+        }
+    });
+
+    document.getElementById('refresh').addEventListener('click', refresh);
+
+    function refresh() {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => show(data));
+    }
+
+    function show(data) {
+        const messages = Object.values(data);
+        messagesArray = [];
+        for (const { author, content } of messages) {
+            if (author && content) {
+                messagesArray.push(`${author}: ${content}`);
+            }
+        }
+        messagesArea.value = messagesArray.join('\n');
+        messagesArea.scrollTop = messagesArea.scrollHeight;
+    }
 }
 
 attachEvents();
