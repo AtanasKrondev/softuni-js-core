6ed27be505377c5d8780d0d1c6f322841d4a5420
Atanas Krondev <krondev@gmail.com>
2019-07-14 12:29:48 +0300

    Update app.js

--- js-apps/01-REST-Services-And-AJAX/exercise/03.PHONEBOOK/app.js
+++ js-apps/01-REST-Services-And-AJAX/exercise/03.PHONEBOOK/app.js
@@ -1,32 +1,58 @@
 function attachEvents() {
+    const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
     document.getElementById('btnLoad')
-        .addEventListener('click', loadList);
+        .addEventListener('click', loadPhonebook);
 
-    function loadList() {
+    document.getElementById('btnCreate')
+        .addEventListener('click', createContact);
+
+    function loadPhonebook() {
         document.getElementById('phonebook').innerHTML = '';
-        let url = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
 
         fetch(url)
             .then((request) => request.json())
             .then((data) => {
-                let values = Object.values(data);
+                const entries = Object.entries(data)
 
-                for (let value of values) {
-                    let name = value.person;
-                    let phoneNumber = value.phone;
+                for (const [key, value] of entries) {
+                    const name = value.person;
+                    const phoneNumber = value.phone;
 
-                    let delBtn = document.createElement('button');
+                    const delBtn = document.createElement('button');
+
                     delBtn.textContent = 'Delete';
+                    delBtn.addEventListener('click', function () {
+                        const delUrl = `https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`;
+                        fetch(delUrl, {
+                            method: 'delete'
+                        }).then(() => loadPhonebook());
+                    });
 
-                    let listItem = document.createElement('li');
+                    const listItem = document.createElement('li');
                     listItem.textContent = `${name}: ${phoneNumber}`;
-
                     listItem.appendChild(delBtn);
-
                     document.getElementById('phonebook').appendChild(listItem);
                 }
             })
     }
+
+    function createContact() {
+        const person = document.getElementById('person').value;
+        const phone = document.getElementById('phone').value;
+        if (person && phone) {
+            const data = { person, phone };
+            fetch(url, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            }).then(() => {
+                loadPhonebook();
+                document.getElementById('person').value = '';
+                document.getElementById('phone').value = '';
+            });
+        }
+    }
+
 }
 
 attachEvents();
