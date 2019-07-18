ecf6cece172586c182625cf287e4e9db7aeaba12
Atanas Krondev <krondev@gmail.com>
2019-07-18 11:46:32 +0300

    Update app.js

--- js-apps/02-Asynchronous-Programming/lab/01.GITHUB-COMMITS/app.js
+++ js-apps/02-Asynchronous-Programming/lab/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,40 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const commits = document.getElementById('commits');
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(handler)
+        .then(showRepos)
+
+
+    function handler(response) {
+        if (response.status > 400) {
+            const text = `Error: ${response.status} (${response.statusText})`;
+            commits.innerHTML = '';
+            commits.appendChild(createHTMLElement('li', text));
+            throw new Error(`Error: ${response.status} (${response.statusText})`)
+        }
+
+        return (response.json());
+    }
+
+    function createHTMLElement(tagName, textContent) {
+        const currentElement = document.createElement(tagName);
+
+        if (textContent) {
+            currentElement.textContent = textContent;
+        }
+
+        return currentElement;
+    }
+
+    function showRepos(data) {
+        commits.innerHTML = '';
+        for (const repo of data) {
+            const text = `${repo.commit.author.name}: ${repo.commit.message}`
+            commits.appendChild(createHTMLElement('li', text));
+        }
+    }
 }
