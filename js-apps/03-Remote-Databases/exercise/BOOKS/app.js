18ebb12a17962e1d7e151b5d30f7146aefc0d630
Atanas Krondev <krondev@gmail.com>
2019-07-22 10:47:50 +0300

    books

--- js-apps/03-Remote-Databases/exercise/BOOKS/app.js
+++ js-apps/03-Remote-Databases/exercise/BOOKS/app.js
@@ -7,6 +7,8 @@ const baseUrl = `https://baas.kinvey.com/appdata/${appKey}/books`;
 const elements = {
     btnSubmit: document.querySelector('form button'),
     btnLoadBooks: document.getElementById('loadBooks'),
+    btnCancelEdit: document.getElementById('cancelBtn'),
+    btnDoneEdit: document.getElementById('editBtn'),
     inputTitle: document.getElementById('title'),
     inputAuthor: document.getElementById('author'),
     inputIsbn: document.getElementById('isbn'),
@@ -87,7 +89,84 @@ function loadEditForm(bookId) {
     elements.inputAuthor.value = dataToEdit[1].textContent;
     elements.inputIsbn.value = dataToEdit[2].textContent;
     elements.h3Form.textContent = 'EDIT BOOK';
-    //-23:52
+    elements.btnSubmit.style.display = 'none';
+    elements.btnDoneEdit.style.display = 'block';
+    elements.btnCancelEdit.style.display = 'block';
+
+    elements.btnDoneEdit.value = bookId;
+    elements.btnDoneEdit.addEventListener('click', editBook);
+    elements.btnCancelEdit.addEventListener('click', cancelEdit);
+}
+
+function editBook(ev) {
+    ev.preventDefault();
+
+    const bookId = ev.target.value;
+    ev.target.value = '';
+
+    const bookData = {
+        'title': elements.inputTitle.value,
+        'author': elements.inputAuthor.value,
+        'isbn': elements.inputIsbn.value,
+    };
+
+    const editUrl = `${baseUrl}/${bookId}`;
+
+    const headers = {
+        method: 'PUT',
+        body: JSON.stringify(bookData),
+        credentials: 'include',
+        Authorization: 'Kinvey' + localStorage.getItem('authToken'),
+        headers: {
+            "Content-type": "application/json"
+        },
+    };
+
+    fetch(editUrl, headers)
+        .then(handler)
+        .then(loadBooks)
+        .catch(err => console.log(err));
+
+    fromEditToSubmitForm();
+}
+
+function cancelEdit(ev) {
+    ev.preventDefault();
+    fromEditToSubmitForm();
+}
+
+function fromEditToSubmitForm() {
+    clearElementValue(elements.inputAuthor, elements.inputTitle, elements.inputIsbn);
+
+    elements.h3Form.textContent = 'FORM';
+
+    elements.btnSubmit.style.display = 'block';
+    elements.btnDoneEdit.style.display = 'none';
+    elements.btnCancelEdit.style.display = 'none';
+}
+
+function clearElementValue(...arguments) {
+    arguments.forEach(element => {
+        element.value = '';
+    })
+}
+
+function deleteBook(bookId) {
+    const deleteUrl = `${baseUrl}/${bookId}`;
+
+    const headers = {
+        method: 'DELETE',
+        credentials: 'include',
+        Authorization: 'Kinvey' + localStorage.getItem('authToken'),
+        headers: {
+            "Content-type": "application/json"
+        },
+    }
+
+    fetch(deleteUrl, headers)
+        .then(handler)
+        .then(loadBooks)
+        .catch(err => console.log(err))
 }
 
 function handler(response) {
