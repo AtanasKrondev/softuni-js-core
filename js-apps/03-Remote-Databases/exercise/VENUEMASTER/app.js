a4b29bc46d7ef890df226ddb0d097fe362ab7e7b
Atanas Krondev <krondev@gmail.com>
2019-07-25 10:13:44 +0300

    Update app.js

--- js-apps/03-Remote-Databases/exercise/VENUEMASTER/app.js
+++ js-apps/03-Remote-Databases/exercise/VENUEMASTER/app.js
@@ -4,8 +4,9 @@
     const appKey = 'kid_BJ_Ke8hZg';
 
     const url = {
-        post: `https://baas.kinvey.com/rpc/kid_BJ_Ke8hZg/custom/calendar?query=`,
-        get: `https://baas.kinvey.com/appdata/kid_BJ_Ke8hZg/venues/`,
+        post: 'https://baas.kinvey.com/rpc/kid_BJ_Ke8hZg/custom/calendar?query=',
+        get: 'https://baas.kinvey.com/appdata/kid_BJ_Ke8hZg/venues/',
+        buy: 'https://baas.kinvey.com/rpc/kid_BJ_Ke8hZg/custom/purchase?venue=',
     }
 
     const element = {
@@ -86,6 +87,8 @@
         element.venueInfo.appendChild(venueDiv);
         const moreInfoBtn = venueDiv.querySelector('input.info');
         moreInfoBtn.addEventListener('click', showMoreInfo)
+        const purchaseBtn = venueDiv.querySelector('input.purchase');
+        purchaseBtn.addEventListener('click', purchaseTickets);
     }
 
     function showMoreInfo(event) {
@@ -95,6 +98,48 @@
         } else if (detailsDiv.style.display === 'block') {
             detailsDiv.style.display = 'none';
         }
+    }
+
+    function purchaseTickets(event) {
+        const id = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+        const qty = event.target.parentNode.parentNode.querySelector('select.quantity').value;
+
+        const headers = {
+            credentials: 'include',
+            Authorization: 'Basic ' + btoa(`${user}:${password}`),
+        }
+
+        fetch(url.get + id, headers)
+            .then(handler)
+            .then(data => showConfirmation(data, qty, id))
+    }
+
+    function showConfirmation(data, qty, id) {
+        const { name, price } = data;
+        element.venueInfo.innerHTML = `
+        <span class="head">Confirm purchase</span>
+        <div class="purchase-info">
+            <span>${name}</span>
+            <span>${qty} x ${price}</span>
+            <span>Total: ${+qty * +price} lv</span>
+            <input type="button" value="Confirm">
+        </div>`;
+
+        const confirmBtn = element.venueInfo.querySelector('input[type="button"]');
+        confirmBtn.addEventListener('click', () => {
+            const headers = {
+                method: "POST",
+                credentials: 'include',
+                Authorization: 'Basic ' + btoa(`${user}:${password}`),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            }
+
+            fetch(`${url.buy}${id}&qty=${qty}`, headers)
+                .then(handler)
+                .then(data => element.venueInfo.innerHTML = '<p>You may print this page as your ticket</p>' + data.html)
+        });
     }
 
     function handler(response) {
