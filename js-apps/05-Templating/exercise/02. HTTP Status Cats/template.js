6a083df36bde042f9cea3daabae3c7df660ca382
Atanas Krondev <krondev@gmail.com>
2019-07-27 12:27:46 +0300

    Update template.js

--- js-apps/05-Templating/exercise/02. HTTP Status Cats/template.js
+++ js-apps/05-Templating/exercise/02. HTTP Status Cats/template.js
@@ -5,22 +5,22 @@
         const template = document.getElementById('cat-template').innerHTML;
         const compiled = Handlebars.compile(template);
         const rendered = compiled({ cats: window.cats });
-        document.getElementById('allCats').innerHTML = rendered;
-        const showBtn = [...document.getElementsByClassName('showBtn')];
-
-        showBtn.forEach(btn => btn.addEventListener('click', showInfo));
+        const allCats = document.getElementById('allCats');
+        allCats.innerHTML = rendered;
+        allCats.addEventListener('click', showInfo);
     }
 
     function showInfo() {
-        const clickedBtn = event.target;
-        const moreInfo = clickedBtn.parentNode.querySelector('div.status');
-        if (clickedBtn.textContent === 'Show status code') {
-            clickedBtn.textContent = 'Hide status code';
-            moreInfo.style.display = '';
-        } else if (clickedBtn.textContent === 'Hide status code') {
-            clickedBtn.textContent = 'Show status code';
-            moreInfo.style.display = 'none';
+        if (event.target.className === 'showBtn') {
+            const clickedBtn = event.target;
+            const moreInfo = clickedBtn.parentNode.querySelector('div.status');
+            if (clickedBtn.textContent === 'Show status code') {
+                clickedBtn.textContent = 'Hide status code';
+                moreInfo.style.display = '';
+            } else if (clickedBtn.textContent === 'Hide status code') {
+                clickedBtn.textContent = 'Show status code';
+                moreInfo.style.display = 'none';
+            }
         }
     }
-
-})()
+})();
